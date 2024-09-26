b5c1b7565fcfdf914f3afd075a75be5c901cd087
Simran <simranjeet.singhchawla@microfocus.com>
2024-09-26 11:05:49 +0530

    1. Introduced depth while cloning and updates. This will help in handling large git history 2. Changed strategy to do an update from git pull to instead git fetch plus git reset

--- config.py
+++ config.py
@@ -17,5 +17,14 @@ SYNC_ENABLED = os.getenv('SYNC_ENABLED', 'true').lower() == 'true'
 
 BASE_REPO_DIR = os.getenv('BASE_REPO_DIR', '/app/repos')
 
+# Fetch the depth value from the environment
+GIT_DEPTH = os.getenv('GIT_DEPTH', None)  # If not provided, defaults to None
+
+if GIT_DEPTH:
+    try:
+        GIT_DEPTH = int(GIT_DEPTH)
+    except ValueError:
+        print("Invalid GIT_DEPTH value, falling back to full clone.")
+        GIT_DEPTH = None
 # Logging Configuration
 # LOG_DIR = os.getenv('LOG_DIR', '/logs')
--- repo_cloner.py
+++ repo_cloner.py
@@ -1,8 +1,10 @@
 import os
+import shutil
 import subprocess
 from logger import log_info, log_error, log_debug, log_warning, log_critical, log_exception
 from git import Repo, InvalidGitRepositoryError, GitCommandError
-from config import CLONE_METHOD
+from config import (CLONE_METHOD, GIT_DEPTH)
+
 
 class RepoCloner:
     def __init__(self):
@@ -14,17 +16,14 @@ class RepoCloner:
         repo_dir = os.path.join(base_dir, repo_name)
         if not os.path.exists(repo_dir):
             try:
-                log_info(f"Cloning repository {repo_name}")
-                Repo.clone_from(clone_url, repo_dir, branch=repo_info['default_branch'], single_branch=True)
+                self.git_clone(clone_url, repo_dir, repo_info, repo_name)
             except GitCommandError as e:
                 log_info(f"Failed to clone {repo_name}: {e}")
                 if os.path.exists(repo_dir):
                     os.rmdir(repo_dir)
         else:
             try:
-                repo = Repo(repo_dir)
-                origin = repo.remotes.origin
-                origin.pull()
+                self.get_fetch_and_reset(repo_dir, repo_info)
                 log_info(f"Updated repository {repo_name}")
             except InvalidGitRepositoryError:
                 log_warning(f"Invalid repository at {repo_dir}, recloning.")
@@ -32,6 +31,33 @@ class RepoCloner:
                 self.clone_or_update_repo(repo_info, base_dir)
             except GitCommandError as e:
                 log_error(f"Failed to update {repo_name}: {e}")
+                self.handle_repo_failure(repo_info, base_dir, repo_dir)
+
+    def get_fetch_and_reset(self, repo_dir, repo_info):
+        repo = Repo(repo_dir)
+        origin = repo.remotes.origin
+        origin.fetch(depth=GIT_DEPTH)  # Fetch with depth if provided
+        default_branch = repo_info['default_branch']
+        repo.git.reset('--hard', f'origin/{default_branch}')
+
+    def git_clone(self, clone_url, repo_dir, repo_info, repo_name):
+        if GIT_DEPTH:
+            log_info(f"Cloning repository {repo_name} with depth of {GIT_DEPTH}")
+            Repo.clone_from(
+                clone_url, repo_dir, branch=repo_info['default_branch'], single_branch=True, depth=GIT_DEPTH)
+        else:
+            log_info(f"Cloning repository {repo_name}")
+            Repo.clone_from(clone_url, repo_dir, branch=repo_info['default_branch'], single_branch=True)
+
+    def handle_repo_failure(self, repo_info, base_dir, repo_dir):
+        # Deletes the repo and reclones in case of failure
+        print(f"Removing faulty repository: {repo_dir}")
+        try:
+            shutil.rmtree(repo_dir)
+            # Reclone after deleting the faulty repo
+            self.clone_or_update_repo(repo_info, base_dir)
+        except Exception as e:
+            print(f"Failed to repair repository: {repo_dir}, error: {e}")
 
     def clone_gitlab_repo(self, repo_info, base_dir):
         # Extract the namespace path from the repository info
@@ -46,17 +72,14 @@ class RepoCloner:
         if not os.path.exists(repo_path):
             os.makedirs(os.path.dirname(repo_path), exist_ok=True)
             try:
-                log_info(f"Cloning GitLab repository {repo_name}")
-                Repo.clone_from(clone_url, repo_path, branch=repo_info['default_branch'], single_branch=True)
+                self.git_clone(clone_url, repo_path, repo_info, repo_name)
             except GitCommandError as e:
                 log_error(f"Failed to clone {repo_name}: {e}")
                 if os.path.exists(repo_path):
                     os.rmdir(repo_path)
         else:
             try:
-                repo = Repo(repo_path)
-                origin = repo.remotes.origin
-                origin.pull()
+                self.get_fetch_and_reset(repo_path, repo_info)
                 log_info(f"Updated GitLab repository {repo_name}")
             except InvalidGitRepositoryError:
                 log_warning(f"Invalid repository at {repo_path}, recloning.")
@@ -64,4 +87,4 @@ class RepoCloner:
                 self.clone_gitlab_repo(repo_info, base_dir)
             except GitCommandError as e:
                 log_error(f"Failed to update {repo_name}: {e}")
-
+                self.handle_repo_failure(repo_info, base_dir, repo_path)
